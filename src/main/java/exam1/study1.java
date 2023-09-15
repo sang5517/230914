f855022b3b5cdb1d5c78ed3b13cf27e004c8c423
sang5517 <sang5517@nate.com>
2023-09-15 17:49:32 +0900

    0915studyilha

--- src/main/java/exam1/study1.java
+++ src/main/java/exam1/study1.java
@@ -7,39 +7,33 @@ import java.util.Scanner;
 
 public class study1 {
     public static void main(String[] args) {
-        ArrayList<String> titles = new ArrayList<>();
-        ArrayList<String> contents = new ArrayList<>();
 
         Scanner scan = new Scanner(System.in);
 
         while (true) {
-            System.out.print("명령어 : ");
+            System.out.print("명령어 :");
             String commend = scan.nextLine();
             if (commend.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             } else if (commend.equals("add")) {
-                System.out.print("게시물 제목을 입력해주세요 : ");
-                 titles.add(scan.nextLine());
-                System.out.print("게시물 내용을 입력해주세요 :");
-                 contents.add(scan.nextLine());
+                String title = scan.nextLine();
+                System.out.print("게시물 제목을 입력해주세요 :");
+                String content = scan.nextLine();
+                System.out.println("게시물 내용을 입력해주세요 :");
 
-                System.out.println("게시물이 등록되었습니다.");
+                System.out.println("게시물이 등록되었습니다");
             } else if (commend.equals("list")) {
-                System.out.println("===========");
-                for (int i = 0; i < titles.size(); i++) {
-                    System.out.printf("제목 : %s\n", titles.get(i));
-                    System.out.printf("내용 : %s\n", contents.get(i));
-                    System.out.println("===========");
+                System.out.println("================");
+                for(int i = 0; i < titles.size(); i++){
+                    String title = scan.nextLine();
+                    System.out.println("내용 : ");
+                    String content = scan.nextLine();
                 }
-            } else if (commend.equals("update")){
-                System.out.print("수정할 게시물 번호 : ");
-                int target = scan.nextInt();
-
-
-
             }
+
 
         }
     }
 }
+
